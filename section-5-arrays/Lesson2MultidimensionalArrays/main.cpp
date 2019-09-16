b64f2e54d997cb53e17d0031fec1c24a1d2e11b4
Brandon <brandoncyu@gmail.com>
2019-09-15 22:24:36 -0700

    added multidimensional arrays

--- section-5-arrays/Lesson2MultidimensionalArrays/main.cpp
+++ section-5-arrays/Lesson2MultidimensionalArrays/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int biArray[3][4] = {0};
+
+    cout << &biArray[0] << endl;
+    cout << &biArray[0][0] << endl;
+    cout << &biArray[0][1] << endl;
+    cout << &biArray[0][2] << endl;
+    cout << &biArray[0][3] << endl;
+    cout << &biArray[1] << endl;
+    cout << &biArray[1][0] << endl;
+
 }
