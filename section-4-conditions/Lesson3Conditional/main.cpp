b89ae692fce45b24458e8082b6d235d08740d344
Brandon <brandoncyu@gmail.com>
2019-09-15 21:27:40 -0700

    added conditional

---
+++ section-4-conditions/Lesson3Conditional/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 30;
+    int b = 40;
+
+    //CONDITION
+    string message = (a > b) ? "a > b" : "a <= b";
+
+    cout << message << endl;
+
+    cout << ((7 < 4) ? 7 : 4) << endl;
+}
