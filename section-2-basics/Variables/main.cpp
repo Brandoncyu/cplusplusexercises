d8b5eed2db0c203d6096d89338e765182e2b8520
Brandon <brandoncyu@gmail.com>
2019-09-15 18:13:07 -0700

    added variables

---
+++ section-2-basics/Variables/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+//variables
+//INITIALIZATION
+//address
+//allocate
+
+int main()
+{
+    int A = 4;
+
+    //A = 4;
+    //& (ampersand)
+    //&name_of_varaible
+
+    cout << A <<endl;
+    cout << &A<<endl;
+
+    A = 10;
+
+    cout << A <<endl;
+    cout << &A;
+
+    /*
+        1. Variables can't have the same name
+        2. Variables can't start from the number
+        3. Can't use spaces.
+        4. Our varaiables should be self-description.
+        5. Variables can't have special keywords or characters.
+        6. Variables should be nouns.
+    */
+}
