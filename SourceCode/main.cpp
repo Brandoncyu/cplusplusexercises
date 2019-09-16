8e640d72ab4203d198b80aae5febc206a074dc48
Brandon <brandoncyu@gmail.com>
2019-09-15 18:00:46 -0700

    added more

---
+++ SourceCode/main.cpp
@@ -0,0 +1,11 @@
+//iostream stands for input output stream.
+#include <iostream>
+
+using namespace std;
+
+//function
+int main()
+{
+    //cout stands for console out. The << points to cout.
+    cout << "This is test";
+}
