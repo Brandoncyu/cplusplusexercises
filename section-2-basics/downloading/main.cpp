047aa052ea2e7d8624c8679148c0dee96fbdbf5a
Brandon <brandoncyu@gmail.com>
2019-09-15 18:48:58 -0700

    added downloading

---
+++ section-2-basics/downloading/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;//console input
+
+    cout << "varaibles names a = " << a << endl;
+
+    string name, surname;
+    cout << "Enter your name: " << endl;
+    cin >> name;
+
+    cout << "Enter your surname: " << endl;
+    cin >> surname;
+
+    cout << "Welcome " << name << " " << surname;
+}
